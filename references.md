f318d4d25660828aaacc18670436db3d68cb3edf
howshekilledit <mgreen@allegheny.edu>
2024-03-05 13:28:34 -0600

    fascinating X 3d

---
+++ references.md
@@ -0,0 +1,2 @@
+![alt text](image.png)
+https://www.google.com/search?sca_esv=6b6104bd345e318c&q=isometric+causality+graphic&tbm=isch&source=lnms&prmd=ihvsnbmtz&sa=X&ved=2ahUKEwiI3YP36d2EAxVOl4kEHTKbBesQ0pQJegQIDBAB&biw=592&bih=779&dpr=2#imgrc=JJ6piaJ5tP2akM
--- sketch.js
+++ sketch.js
@@ -1,17 +1,44 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
+  noLoop(); 
 }
 
 function draw() {
-  background(220);
-  drawPolarRombus(30, 100);
+  background(255);
+  //drawPolarRombus(30, 100);
+  //drawPolarPrism();
+  p = new Prism(100, 100, 100);
+  p.draw();
+
+}
+
+function drawAxes(){
   // draw axis lines
   line(width/2, 0, width/2, height);
   line(0, height/2, width, height/2);
 }
 
-function drawPolarRombus(a, d, origin = createVector(width/2, height/2)){
+class Prism{
+  constructor(l, w, h, origin = createVector(width/2, height/2)){
+    this.top = polarRhombus(30, 100, origin);
+    this.bottom = polarRhombus(30, 100, origin.add(createVector(0, h)));
+  }
+  draw(){
+    beginShape();
+    for (let p of this.top){
+      vertex(p.x, p.y);
+    }
+    for (let p of this.bottom){
+      vertex(p.x, p.y);
+    }
+    endShape(CLOSE);
+  }
+
+}
+
+
+function polarRhombus(a, d, origin = createVector(width/2, height/2)){
   // draw a line that is a degrees from origin
   // and d units long
 
@@ -26,10 +53,7 @@ function drawPolarRombus(a, d, origin = createVector(width/2, height/2)){
   for (let p of pts){
     p.add(origin);
   } 
-  for (let [i, p] of pts.entries()){
-    np = pts[(i+1) % pts.length];
-    line(p.x, p.y, np.x, np.y);
-  }
-
+  return pts; 
 
 }
+
