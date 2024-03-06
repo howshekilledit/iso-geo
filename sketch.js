9557a889f1e46922062bf5eb612f74f2aeccea26
howshekilledit <mgreen@allegheny.edu>
2024-03-06 12:04:11 -0600

    cool optics old problems

--- sketch.js
+++ sketch.js
@@ -1,17 +1,23 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
+
+  noFill();
   noLoop(); 
 }
 
 function draw() {
   background(255);
-  //drawPolarRombus(30, 100);
-  //drawPolarPrism();
-  for (let i = 0; i < 10; i++){
-    let p = new Prism(100, 100, 100, origin = createVector(100 + i*200, 100 + i*200));
-    p.draw();
-  }
+  
+
+  let p = new Prism(50, 100, 50);
+  p.draw();
+  r = rhombus(30, 50);
+  //drawShape(r);
+  // for (let i = 0; i < 10; i++){
+  //   let p = new Prism(100, 100, 100, ogn = createVector(100 + i*200, 100 + i*200));
+  //   p.draw();
+  // }
 
 }
 
@@ -22,9 +28,9 @@ function drawAxes(){
 }
 
 class Prism{
-  constructor(l, w, h, origin = createVector(width/2, height/2)){
-    this.top = polarRhombus(30, 100, origin);
-    this.bottom = polarRhombus(30, 100, origin.add(createVector(0, h)));
+  constructor(l, w, h,  ogn = createVector(width/2, height/2)){
+    this.top = rhombus(30, l, ogn);
+    this.bottom = rhombus(30, l, ogn.add(createVector(0, h)));
     this.left = [this.top[0].copy(), this.bottom[0].copy(), this.bottom[3].copy(), this.top[3].copy()];
     this.right = [this.top[0].copy(), this.bottom[0].copy(), this.bottom[1].copy(), this.top[1].copy()];
   }
@@ -45,7 +51,25 @@ function drawShape(pts){
   endShape(CLOSE);
 }
 
-function polarRhombus(a, d, origin = createVector(width/2, height/2)){
+// a is the known side (adjascent to angle)
+function rhombus(angle, hyp, ogn = createVector(width/2, height/2)){
+  // draw a rhombus with an angle a, width and hypotenuse
+  console.log(ogn);
+  // a is the adjacent side
+  a = cos(angle) * hyp;
+  // b is the opposite side
+  b = sin(angle) * hyp;
+  p1 = createVector(0,0).add(ogn);
+  p2 = createVector(a, 0).add(ogn);
+  p3 = createVector(2 * a, b).add(ogn);
+  p4 = createVector(a, b).add(ogn);
+  //get distance between p1 and p4
+  d = dist(p2.x, p2.y, p3.x, p3.y);
+  print(d);
+  return [p1, p2, p3, p4];
+}
+
+function isoUnit(a, d, ogn = createVector(width/2, height/2)){
   // draw a line that is a degrees from origin
   // and d units long
 
@@ -58,7 +82,7 @@ function polarRhombus(a, d, origin = createVector(width/2, height/2)){
     createVector(-abs_x, 0)
   ]
   for (let p of pts){
-    p.add(origin);
+    p.add(ogn);
   } 
   return pts; 
 
